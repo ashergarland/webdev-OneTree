2eaa5aec50076aa4fec974f95d7f5c46963f70ed
Asher Garland <asher.garland@gmail.com>
2017-07-15 12:09:25 -0700

    Implemented responsive design using media queries

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     scrollToInit();
+    activeSectionNavigation();
     InitWaypointAnimations({
         offset: "60%",
         animateClass: "wp-animated",
@@ -16,3 +17,54 @@ function scrollToInit() {
         event.preventDefault();
     });
 }
+
+function activeSectionNavigation() {
+    function getNavItemsMap() {
+        const navItemsMap = $("#main-nav").find(".nav-item").map((index, item) => {
+            const $item = $(item);
+            const name = $item.find(".nav-link").attr("href").substring(1);
+            return {
+                key: name,
+                val: $item
+            };
+        })
+        .toArray()
+        .reduce((map, obj) => {
+            map[obj.key] = obj.val;
+            return map;
+        }, {});
+
+        return navItemsMap;
+    }
+
+    function deactivateCurrentNavItem() {
+        $("#main-nav").find(".nav-item.active").removeClass("active");
+    }
+    
+    const navItemsMap = getNavItemsMap();
+    $("section").each((index, element) => {
+        const $element = $(element);
+        const sectionName = $element.attr("id");
+        if(sectionName in navItemsMap) {
+            
+            $element.waypoint((direction) => {
+                if(direction === "down") {
+                    deactivateCurrentNavItem();
+                    navItemsMap[sectionName].addClass("active");
+                }
+            },{
+                offset: "50%"
+            });
+            
+            $element.waypoint((direction) => {
+                if(direction === "up") {
+                    deactivateCurrentNavItem();
+                    navItemsMap[sectionName].addClass("active");
+                }
+            },{
+                offset: "-20%"
+            })
+        }
+    });
+}
+
